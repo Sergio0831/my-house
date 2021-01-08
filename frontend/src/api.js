1d0349a6b000b391e57c8982d103e2c5fed82a66
Sergejs Ivcenko <sergejs.ivcenko@gmail.com>
2021-01-08 09:07:11 +0000

    Created register screen UI and implemented register screen action

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -17,12 +17,35 @@ export const getProduct = async (id) => {
   }
 };
 
+/* Login Screen */
 export const signin = async ({ email, password }) => {
   try {
     const response = await axios({
       url: `${apiUrl}/api/users/signin`,
       method: "POST",
       data: {
+        email,
+        password,
+      },
+    });
+    if (response.statusText !== "OK") {
+      throw new Error(response.data.message);
+    }
+    return response.data;
+  } catch (err) {
+    console.log(err);
+    return { error: err.response.data.message || err.message };
+  }
+};
+
+/* Register Screen */
+export const register = async ({ name, email, password }) => {
+  try {
+    const response = await axios({
+      url: `${apiUrl}/api/users/register`,
+      method: "POST",
+      data: {
+        name,
         email,
         password,
       },
